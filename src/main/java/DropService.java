fbb611e2aa293c2938fa18af97bfc5811ad51dc8
Mattias Johansson <mattias.johansson@atex.com>
2014-08-21 23:11:02 +0200

    Minor refactoring

--- src/main/java/DropService.java
+++ src/main/java/DropService.java
@@ -2,38 +2,35 @@ import org.apache.tika.Tika;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
-@Path("")
+import static java.lang.String.format;
+
+@javax.ws.rs.Path("{filename}")
 public class DropService
 {
 
+    private static final String STORAGE_DIR = "/tmp/dropservice/";
+
     @GET
-    @Path("{filename}")
-    public Response hello(@PathParam("filename") String filename)
-            throws Exception
-    {
-        java.nio.file.Path path = Paths.get("/tmp/dropservice/" + filename);
+    public Response download(@PathParam("filename") String filename) throws Exception {
+        Path path = Paths.get(STORAGE_DIR + filename);
         String mimetype = new Tika().detect(path.toFile());
         return Response.ok(Files.newInputStream(path), mimetype).build();
     }
 
     @POST
-    @Path("{filename}")
-    public Response formPost(@PathParam("filename") String filename, InputStream data)
-            throws Exception
-    {
-        if (new File("/tmp/dropservice/" + filename).exists()) {
-            filename = String.format("%s=%s", UUID.randomUUID().toString(), filename);
+    public Response upload(@PathParam("filename") String filename, InputStream data) throws Exception {
+        if (Files.exists(Paths.get(STORAGE_DIR + filename))) {
+            filename = format("%s-%s", UUID.randomUUID().toString(), filename);
         }
-        Files.copy(data, Paths.get("/tmp/dropservice/" + filename));
+        Files.copy(data, Paths.get(STORAGE_DIR + filename));
         return Response.ok(filename).build();
     }
 
