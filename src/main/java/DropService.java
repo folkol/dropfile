64522164ecde7f92e1d4d1f744f08514e8f7b862
Matte <mattias4@kth.se>
2016-05-08 18:28:06 +0200

    Added PUT support. Can now also overwrite previous files.

--- src/main/java/DropService.java
+++ src/main/java/DropService.java
@@ -2,6 +2,7 @@ import org.apache.tika.Tika;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
@@ -10,27 +11,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
-import static java.lang.String.format;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-@javax.ws.rs.Path("{filename}")
+@javax.ws.rs.Path("")
 public class DropService
 {
-
     private static final String STORAGE_DIR = "/tmp/dropservice/";
+    private static final Tika TIKA = new Tika();
 
     @GET
+    @javax.ws.rs.Path("{filename}")
     public Response download(@PathParam("filename") String filename) throws Exception {
         Path path = Paths.get(STORAGE_DIR + filename);
-        return Response.ok(Files.newInputStream(path), new Tika().detect(path.toFile())).build();
+        return Response.ok(Files.newInputStream(path), TIKA.detect(path.toFile())).build();
     }
 
     @POST
-    public Response upload(@PathParam("filename") String filename, InputStream data) throws Exception {
-        if (Files.exists(Paths.get(STORAGE_DIR + filename))) {
-            filename = format("%s-%s", UUID.randomUUID().toString(), filename);
-        }
-        Files.copy(data, Paths.get(STORAGE_DIR + filename));
-        return Response.ok(filename).build();
+    public Response upload(InputStream data) throws Exception {
+        return upload(UUID.randomUUID().toString(), data);
     }
 
+    @PUT
+    @javax.ws.rs.Path("{filename}")
+    public Response upload(@PathParam("filename") String filename, InputStream data) throws Exception {
+        Files.copy(data, Paths.get(STORAGE_DIR + filename), REPLACE_EXISTING);
+        return Response.ok(filename).build();
+    }
 }
